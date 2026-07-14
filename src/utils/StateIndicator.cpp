a2e8a88f2c9091301b4ce645e64f1e16f2709cbc
Oliver Bagge <bagge@teco.edu>
2026-07-14 12:10:32 +0200

    fix(dfu): Disable per-chunk LED flashes during FOTA making uploads more stable

--- src/utils/StateIndicator.cpp
+++ src/utils/StateIndicator.cpp
@@ -9,45 +9,11 @@
 
 #include "channel_assignment.h"
 
-#ifdef CONFIG_MCUMGR_MGMT_NOTIFICATION_HOOKS
-#include <zephyr/mgmt/mcumgr/mgmt/mgmt.h>
-#include <zephyr/mgmt/mcumgr/mgmt/callbacks.h>
-#endif
-
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(state_indicator, CONFIG_LOG_DEFAULT_LEVEL);
 
 ZBUS_CHAN_DECLARE(bt_mgmt_chan);
 ZBUS_CHAN_DECLARE(battery_chan);
-
-#ifdef CONFIG_MCUMGR_MGMT_NOTIFICATION_HOOKS
-
-struct mgmt_callback mcu_mgr_cb;
-
-enum mgmt_cb_return chuck_write_indication(uint32_t event, enum mgmt_cb_return prev_status,
-                                int32_t *rc, uint16_t *group, bool *abort_more,
-                                void *data, size_t data_size)
-{
-    if (event == MGMT_EVT_OP_IMG_MGMT_DFU_CHUNK) {
-        /* This is the event we registered for */
-		led_controller.setColor(LED_ORANGE);
-        k_msleep(10);
-        led_controller.setColor(LED_OFF);
-    }
-    /*else if (event == MGMT_EVT_OP_IMG_MGMT_DFU_CHUNK_WRITE_COMPLETE) {
-        led_controller.setColor(LED_OFF);
-    }
-    else if (event == MGMT_EVT_OP_OS_MGMT_RESET) {
-		LOG_INF("RESET received");
-	}*/
-
-	//LOG_DBG("mcu mgr hook called with event: %d", event);
-
-    /* Return OK status code to continue with acceptance to underlying handler */
-    return MGMT_CB_OK;
-}
-
-#endif
 
 static void connect_evt_handler(const struct zbus_channel *chan)
 {
@@ -93,12 +59,6 @@ void StateIndicator::init(struct earable_state state) {
 	if (ret && ret != -EALREADY) {
 		LOG_ERR("Failed to add battery listener");
 	}
-
-#ifdef CONFIG_MCUMGR_MGMT_NOTIFICATION_HOOKS 
-	mcu_mgr_cb.callback = chuck_write_indication;
-    mcu_mgr_cb.event_id = MGMT_EVT_OP_IMG_MGMT_DFU_CHUNK; //MGMT_EVT_OP_IMG_MGMT_ALL
-    mgmt_callback_register(&mcu_mgr_cb);
-#endif
 
     set_state(state);
 }
